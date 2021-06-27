f77d466caaa9812d1c0f3a5d61bb7bb1a85c3b6d
Akemin-PC\Akemin <sirokumaorange1129@gamil.com>
2021-06-27 17:51:02 +0900

    編集

---
+++ pages/confirm.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="confirm">
+    <div>
+      <h1>ログイン完了！！</h1>
+      <p>{{user.displayName}}</p>
+      <p>{{user.displayEmail}}</p>
+      <button @click="logout">ログアウト</button>
+    </div>
+  </div>
+</template>
+<script>
+import firebase from "~/plugins/firebase";
+export default {
+  props: ['user'],
+  methods: {
+    logout: function() {
+      firebase.auth().signOut();
+    }
+  }
+}
+</script>
+<style scoped>
+.confirm {
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
